3f786e106b1ba6550c3e5d11964ddce254e1eab8
letusbeus <letusbeus@mail.ru>
2024-06-25 16:51:56 +0300

    - added funcs for date mode - added funcs for message mode - updated 'hello' func - added handlers & patterns for date & message modes

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, CommandHandler, CallbackQueryHandler
 
-from testdata import tg_token, gpt_token, start_menu
+from testdata import tg_token, gpt_token, start_menu, date_partners
 from util import *
 from gpt import *
 
@@ -8,15 +8,15 @@ from gpt import *
 async def start(update, context):
     dialog.mode = "main"
     text = load_message("main")
-    await send_photo(update, context, name="main")
+    await send_photo(update, context, "main")
     await send_text(update, context, text)
-    await show_main_menu(update, context, commands=start_menu)
+    await show_main_menu(update, context, start_menu)
 
 
 async def gpt(update, context):
     dialog.mode = "gpt"
     text = load_message("gpt")
-    await send_photo(update, context, name="gpt")
+    await send_photo(update, context, "gpt")
     await send_text(update, context, text)
 
 
@@ -27,9 +27,65 @@ async def gpt_dialog(update, context):
     await send_text(update, context, answer)
 
 
+async def date(update, context):
+    dialog.mode = "date"
+    text = load_message("date")
+    await send_photo(update, context, "date")
+    await send_text_buttons(update, context, text, date_partners)
+
+
+async def date_dialog(update, context):
+    text = update.message.text
+    my_message = await send_text(update, context, "_Собеседник набирает текст..._")
+    answer = await chatgpt.add_message(text)
+    await my_message.edit_text(answer)
+
+
+async def date_button(update, context):
+    query = update.callback_query.data
+    await update.callback_query.answer()
+
+    await send_photo(update, context, query)
+    await send_text(update, context, "Отличный выбор! Пригласите выбранного партнера на свидание за 5 сообщений :)")
+
+    prompt = load_prompt(query)
+    chatgpt.set_prompt(prompt)
+
+
+async def message(update, context):
+    dialog.mode = 'message'
+    text = load_message('message')
+    await send_photo(update, context, "message")
+    await send_text_buttons(update, context, text, {
+        'message_next': 'Следующее сообщение',
+        'message_date': 'Пригласить на свидание'
+    })
+    dialog.list.clear()
+
+
+async def message_dialog(update, context):
+    text = update.message.text
+    dialog.list.append(text)
+
+
+async def message_button(update, context):
+    query = update.callback_query.data
+    await update.callback_query.answer()
+
+    prompt = load_prompt(query)
+    user_chat_history = "\n\n".join(dialog.list)
+    my_message = await send_text(update, context, "_ChatGPT думает над ответом..._")
+    answer = await chatgpt.send_question(prompt, user_chat_history)
+    await my_message.edit_text(answer)
+
+
 async def hello(update, context):
-    if dialog.mode == "gpt":
+    if dialog.mode == 'gpt':
         await gpt_dialog(update, context)
+    elif dialog.mode == 'date':
+        await date_dialog(update, context)
+    elif dialog.mode == 'message':
+        await message_dialog(update, context)
     else:
         await send_text(update, context, text="*Привет*")
         await send_text(update, context, text="_Как дела?_")
@@ -50,12 +106,17 @@ async def hello_button(update, context):
 
 dialog = Dialog()
 dialog.mode = None
+dialog.list = []
 
 chatgpt = ChatGptService(token=gpt_token)
 app = ApplicationBuilder().token(tg_token).build()
 app.add_handler(CommandHandler('start', start))
-app.add_handler(CommandHandler("gpt", gpt))
+app.add_handler(CommandHandler('gpt', gpt))
+app.add_handler(CommandHandler('date', date))
+app.add_handler(CommandHandler('message', message))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
+app.add_handler(CallbackQueryHandler(date_button, pattern="^date_.*"))
+app.add_handler(CallbackQueryHandler(message_button, pattern="^message_.*"))
 app.add_handler(CallbackQueryHandler(hello_button))
 
 app.run_polling()
